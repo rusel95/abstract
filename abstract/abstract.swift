3f34a259a801e602f7587518cceb89d2328f4e90
Ruslan Popesku <ruslanpopesku95@gmail.com>
2017-11-21 14:48:24 +0200

    first commit

---
+++ abstract/abstract.swift
@@ -0,0 +1,20 @@
+//
+//  abstract.swift
+//  abstract
+//
+//  Created by Admin on 11/21/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
+
+public class Abstract {
+    
+    public static func doSomething() {
+        print("asdjfasd;flkjdsf")
+    }
+    
+    public static func secondFunction() {
+        print("ajskdf;lkajsd;fljadsjf;lkjcxv;lkjq;lfja;ldjf;laskjfads;fkj")
+    }
+}
