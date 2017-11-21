50dcdc97ac9be3fd74fc591d9f13a29b89c6e21d
Ruslan Popesku <ruslanpopesku95@gmail.com>
2017-11-21 13:42:20 +0200

    Initial Commit

---
+++ abstract/abstract.h
@@ -0,0 +1,19 @@
+//
+//  abstract.h
+//  abstract
+//
+//  Created by Admin on 11/21/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for abstract.
+FOUNDATION_EXPORT double abstractVersionNumber;
+
+//! Project version string for abstract.
+FOUNDATION_EXPORT const unsigned char abstractVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <abstract/PublicHeader.h>
+
+
